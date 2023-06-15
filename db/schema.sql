70012a0933c595ff943369d629190809c4ad1bf7
Michael O'Sullivan <michaelhallosullivan@gmail.com>
2023-06-15 12:38:43 -0700

    added seed data and fussed with dotenv

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-DROP DATABASE IF EXISTS grocery_db;
+/*DROP DATABASE IF EXISTS grocery_db;
 CREATE DATABASE grocery_db;
 
 USE grocery_db;
@@ -18,3 +18,33 @@ CREATE TABLE customer_order (
   REFERENCES customers(id)
   ON DELETE SET NULL
 );
+*/
+
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE employees (
+  employee_id INT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  --foreign key here
+  role_id(provides job_title, department_name, salary)
+  --foreign key here
+  manager_id
+  is_manager BOOLEAN NOT NULL
+)
+
+CREATE TABLE roles (
+  role_id INT PRIMARY KEY,
+  job_title VARCHAR(30) NOT NULL,
+  salary DECIMAL(6, 2),
+  --foreign key here
+  department_id(provides department_name)
+)
+
+CREATE TABLE departments (
+  department_id INT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,27 @@
-INSERT INTO customers (id, first_name, last_name)
-VALUES (001, "Arthur", "Miller"),
-       (002, "Chinua", "Achebe"),
-       (003, "Margaret", "Atwood"),
-       (004, "Gabriel", "Garcia Marquez"),
-       (005, "Simone", "de Beauvoir");
-       
-INSERT INTO customer_order (id, customer_id, order_details)
-VALUES (001, 001, "coffee, milk"),
-       (002, 002, "eggs, cheese"),
-       (003, 003, "organic cereal"),
-       (004, 004, "oranges, peaches"),
-       (005, 005, "ice cream");
-       
+INSERT INTO employees (employee id, first_name, last_name, role_id, manager_id)
+    (001, "John", "Doe", 001, null),
+    (002, "Mike", "Chan", 002, 001),
+    (003, "Ashley", "Rodriguez", 003, null),
+    (004, "Kevin", "Tupik", 004, 003),
+    (005, "Kunal", "Singh", 005, null),
+    (006, "Malia", "Brown", 006, 005),
+    (007, "Sarah", "Lourd", 007, null),
+    (008, "Tom", "Allen", 008, 007);
+
+
+
+INSERT INTO roles (role_id, job_title, department_id, salary)
+VALUES (001, "Sales Lead", 001, 100000),
+       (002, "Salesperson", 001, 80000),
+       (003, "Lead Engineer", 002, 150000),
+       (004, "Software Engineer", 002, 120000),
+       (005, "Account Manager", 003, 160000),
+       (006, "Accountant", 003, 125000),
+       (007, "Legal Team Lead", 004, 250000),
+       (008, "Lawyer", 004, 190000);
+
+INSERT INTO departments (department_id, department_name)
+VALUES (001, "Sales"),
+       (002, "Engineering"),
+       (003, "Finance"),
+       (004, "Legal");

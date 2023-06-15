97f7da1bf70aa449609bad8734b8441b93ee4b06
Michael O'Sullivan <michaelhallosullivan@gmail.com>
2023-06-15 12:52:16 -0700

    .gitignore is now working

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL username,
     user: process.env.DB_USER,
-    // MySQL password
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
   },
